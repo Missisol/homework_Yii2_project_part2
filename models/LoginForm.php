f9ca967ab33a11e81f036d9053e5b3a341de41ba
missisol <missisololol@gmail.com>
2019-01-28 06:23:04 +0300

    Added homework 5

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -68,12 +68,12 @@ class LoginForm extends Model
     /**
      * Finds user by [[username]]
      *
-     * @return User|null
+     * @return UserBase|null
      */
     public function getUser()
     {
         if ($this->_user === false) {
-            $this->_user = User::findByUsername($this->username);
+            $this->_user = UserBase::findByUsername($this->username);
         }
 
         return $this->_user;
---
+++ views/user/test.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Test';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+  <h1><?= Html::encode($this->title) ?></h1>
+  <?php Pjax::begin(); ?>
+
+
+  <?php Pjax::end(); ?>
+</div>
+
